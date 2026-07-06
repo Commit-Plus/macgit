305804134bebc98d7cb81a81f3fae4d0873d6b0f
Thanh Tran <trantienthanh2412@gmail.com>
2026-07-06 21:32:55 +0700

    feat: add git provider account models

---
+++ macgitTests/GitProviderAccountModelsTests.swift
@@ -0,0 +1,64 @@
+//
+//  macgit (Commit+) - a macOS Git client built with Swift and SwiftUI.
+//  Copyright (C) 2026  Thanh Tran <trantienthanh2412@gmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import XCTest
+@testable import macgit
+
+final class GitProviderAccountModelsTests: XCTestCase {
+    func testGitHubDotComHostNormalizesToHttpsBaseURL() {
+        XCTAssertEqual(GitProviderHost.githubDotCom.baseURL.absoluteString, "https://github.com")
+    }
+
+    func testSelfHostedGitLabHostPreservesHost() throws {
+        let baseURL = try XCTUnwrap(URL(string: "https://git.company.com/"))
+        let host = GitProviderHost(kind: .gitlab, baseURL: baseURL)
+
+        XCTAssertEqual(host.normalized.baseURL.absoluteString, "https://git.company.com")
+    }
+
+    func testProviderAccountRoundTripsCodable() throws {
+        let account = GitProviderAccount(
+            id: "connection-1",
+            macgitUID: "macgit-user-1",
+            provider: .github,
+            hostURL: try XCTUnwrap(URL(string: "https://github.com")),
+            providerUserID: "provider-user-42",
+            username: "octocat",
+            displayName: "The Octocat",
+            avatarURL: URL(string: "https://avatars.githubusercontent.com/u/583231"),
+            scopes: ["repo", "read:user"],
+            permissions: ["contents": "read", "pull_requests": "write"],
+            tokenStatus: .valid,
+            connectedAt: Date(timeIntervalSince1970: 1_700_000_000),
+            lastValidatedAt: Date(timeIntervalSince1970: 1_700_000_100)
+        )
+
+        let data = try JSONEncoder().encode(account)
+        let decoded = try JSONDecoder().decode(GitProviderAccount.self, from: data)
+
+        XCTAssertEqual(decoded.providerUserID, account.providerUserID)
+        XCTAssertEqual(decoded.username, account.username)
+        XCTAssertEqual(decoded.scopes, account.scopes)
+        XCTAssertEqual(decoded.permissions, account.permissions)
+        XCTAssertEqual(decoded.tokenStatus, account.tokenStatus)
+    }
+
+    func testUnavailableTokenStatusIsDistinctFromRevoked() {
+        XCTAssertNotEqual(GitProviderTokenStatus.unavailableOnThisDevice, .revoked)
+    }
+}
